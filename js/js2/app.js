ac220bbbe535da187ea17daee14af796d1ac80b4
SayemHosen890 <sayemhosen902@gmail.com>
2021-08-21 23:53:00 +0600

    second commit

--- js/js2/app.js
+++ js/js2/app.js
@@ -1,48 +1,74 @@
+const basePrice = document.getElementById('base-price')
+const basePriceText = parseFloat(basePrice.innerText);
+console.log(basePriceText);
+
+
 document.getElementById('8gb-memory').addEventListener('click', function () {
     const firstMemory = document.getElementById('memory-cost');
     firstMemory.innerText = '0';
     const firstValue = parseFloat(firstMemory.innerText);
-    // console.log(firstValue);
+    console.log(firstValue);
 });
 document.getElementById('16gb-memory').addEventListener('click', function () {
     const secondMemory = document.getElementById('memory-cost');
     secondMemory.innerText = '180';
     const secondValue = parseFloat(secondMemory.innerText);
-    // console.log(secondValue);
+    console.log(secondValue);
 })
 document.getElementById('256-storage').addEventListener('click', function () {
     const firstStorage = document.getElementById('extra-storage');
     firstStorage.innerText = '0';
     const thirdValue = parseFloat(firstStorage.innerText);
-    // console.log(thirdValue);
+    console.log(thirdValue);
 })
 document.getElementById('512gb-storage').addEventListener('click', function () {
     const secondStorage = document.getElementById('extra-storage');
     secondStorage.innerText = '100';
     const fourtValue = parseFloat(secondStorage.innerText);
-    // console.log(fourtValue);
+    console.log(fourtValue);
 })
 document.getElementById('1TB-storage').addEventListener('click', function () {
     const thirdStorage = document.getElementById('extra-storage');
     thirdStorage.innerText = '180';
     const fifthValue = parseFloat(thirdStorage.innerText);
-    // console.log(fifthValue);
+    console.log(fifthValue);
 })
 document.getElementById('prime-delivery').addEventListener('click', function () {
     const primeCost = document.getElementById('delivery-charge');
     primeCost.innerText = '0';
     const sixthValue = parseFloat(primeCost.innerText);
-    // console.log(sixthValue);
+    console.log(sixthValue);
 })
 document.getElementById('charge-delivery').addEventListener('click', function () {
     const chargeCost = document.getElementById('delivery-charge');
     chargeCost.innerText = '20';
     const seventhValue = parseFloat(chargeCost.innerText);
-    const mainValue = seventhValue + value;
-    console.log(mainValue);
+    console.log(seventhValue);
+    // const totalValue = document.getElementById('total-price');
+    // const value = parseFloat(totalValue.innerText);
 })
-const totalValue = document.getElementById('total-price');
-const value = parseFloat(totalValue.innerText);
+function checkPromo() {
+    const totalPrice = document.getElementById('total-price');
+    const total = document.getElementById('total-amount');
+    const inputText = document.getElementById('input-text').value;
+    if (inputText == 'stevekaku') {
+        total.innerText = totalPrice.innerText - (totalPrice.innerText * 0.20)
+    }
+    else {
+        alert('provide right code');
+    }
+}
+document.getElementById('apply-promo').addEventListener('click', function () {
+    checkPromo();
+})
+// function updatePrice() {
+//     const totalValue = document.getElementById('total-price');
+//     const value = parseFloat(totalValue.innerText);
+// }
+// const totalValue = document.getElementById('total-price');
+// const value = parseFloat(totalValue.innerText);
+// const mainValue = seventhValue + value;
+// console.log(mainValue);
 // console.log(value);
 
 // function addTotal() {
@@ -50,5 +76,5 @@ const value = parseFloat(totalValue.innerText);
 //     console.log(totalPrice);
 // }
 // console.log(theValue);
-const totalValue = document.getElementById('total-price');
-const value = parseFloat(totalValue.innerText);
+// const totalValue = document.getElementById('total-price');
+// const value = parseFloat(totalValue.innerText);
